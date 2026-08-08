b73de6320ffa368ae5b25bdfa636a03debdd7df0
prashant-m4 <prashantsingh090798@gmail.com>
2026-08-08 17:57:25 +0530

    feat: add endpoint to fetch task details and update DTOs
    - Introduced a new GET endpoint to fetch a single task by ID, returning TaskOneFetchResponse.
    - Updated DTOs to include TaskOneFetchResponse for standardized task detail representation.

--- src/deskmate/api/tasks.py
+++ src/deskmate/api/tasks.py
@@ -1,12 +1,21 @@
 
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from deskmate.dao.tasks import Task
 from deskmate.db import get_db
-from deskmate.dto.tasks import TaskCreateRequest, TaskCreateResponse
+from deskmate.dto.tasks import TaskCreateRequest, TaskCreateResponse, TaskOneFetchResponse
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
+
+@router.get("{task_id}", response_model=TaskOneFetchResponse)
+async def get_project(task_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Task).where(Task.id==task_id))
+    
+    project = result.scalars().one()
+
+    return project
 
 @router.post("", response_model=TaskCreateResponse)
 async def create_task(new_task: TaskCreateRequest, db: AsyncSession = Depends(get_db)):
--- src/deskmate/dto/tasks.py
+++ src/deskmate/dto/tasks.py
@@ -1,5 +1,16 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
+
+class TaskOneFetchResponse(BaseModel):
+    id: int
+    name: str
+    desc: str | None
+    owner: str
+    created_at: datetime
+    updated_at: datetime
+    project_id: int
 
 class TaskCreateRequest(BaseModel):
     name: str
